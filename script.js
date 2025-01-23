50257285219afa4ae13cc546b7d4ca56636035b6
aduong3 <mythalink@gmail.com>
2025-01-23 01:38:06 -0800

    created branch to increase code readability and flow
    main branch's code had a lot of bugs to consider, so this branch is to develop and test it more.

--- script.js
+++ script.js
@@ -14,9 +14,10 @@ function Gameboard() {
 
   const placeMarker = (row, column, marker) => {
     //console.log(board[row][column].getValue());
-    while (board[row][column].getValue() === '') {
-      board[row][column].addMarker(marker);
+    while (board[row][column].getValue() === "") {
+      board[row][column].setValue(marker);
     }
+    //console.log(board[row][column].getValue());
   };
 
   const printBoard = () => {
@@ -27,25 +28,35 @@ function Gameboard() {
   };
 
   return { getBoard, placeMarker, printBoard };
-}
+};
 
 function Cell() {
   let value = "";
 
-  const addMarker = (marker) => {
+  const setValue = (marker) => {
     value = marker;
   };
 
   const getValue = () => value;
 
-  return { addMarker, getValue };
-}
+  return { setValue, getValue };
+};
+
+function turnCounter() {
+  let turn = 1;
+
+  const getTurn = () => turn;
+  const increaseTurn = () => turn++;
+
+  return {getTurn, increaseTurn};
+};
 
 function GameController(
   playerOneName = "Player One",
   playerTwoName = "Player Two"
 ) {
   const board = Gameboard();
+  const turnCount = turnCounter();
 
   const players = [
     {
@@ -59,8 +70,63 @@ function GameController(
   ];
   let activePlayer = players[0];
 
+
+  const checkStatus = () => {
+    const checkBoard = board.getBoard();
+    const marker = getActivePlayer().marker;
+
+
+    let firstDiagCount = 0;
+    let secondDiagCount = 0;
+    //console.log(checkBoard[0][0].getValue());
+    //console.log(turnCount.getTurn());
+    for (let i = 0; i < 3; i++) {
+      if (
+        //rows
+        checkBoard[i][0].getValue() == marker &&
+        checkBoard[i][0].getValue() == checkBoard[i][1].getValue() &&
+        checkBoard[i][0].getValue() == checkBoard[i][2].getValue()
+      ) {
+        //console.log('winner');
+        return "win";
+      }
+      if (
+        //columns
+        checkBoard[0][i].getValue() == marker &&
+        checkBoard[0][i].getValue() == checkBoard[1][i].getValue() &&
+        checkBoard[0][i].getValue() == checkBoard[2][i].getValue()
+      ) {
+        return "win";
+      }
+      if (checkBoard[i][i].getValue() == marker) {
+        //diagonals [0][0],[1][1],[2][2] || [0][2] [1][1] [2][0]; [i][i] || [i][2-i]
+        firstDiagCount++;
+      }
+      if (checkBoard[i][2 - i].getValue() == marker) {
+        secondDiagCount++;
+      }
+
+      if (firstDiagCount == 3 || secondDiagCount == 3) return "win";
+    }
+    if(turnCount.getTurn() === 9){
+      return 'tie';
+    }
+    return null;
+  };
+
+  const isGameOver = () => {
+    const status = checkStatus();
+    //console.log(status);
+    if(status == 'win'){
+      console.log(`${getActivePlayer().name} has won!`);
+    } else if(status == 'tie'){
+      console.log('Tie Game!');
+    }
+  };
+
   const switchPlayerTurn = () => {
     activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    turnCount.increaseTurn();
   };
 
   const getActivePlayer = () => activePlayer;
@@ -70,10 +136,16 @@ function GameController(
   };
 
   const playRound = (row, column) => {
-    board.placeMarker(row, column, getActivePlayer().marker);
+      if(board.getBoard()[row][column].getValue() == ''){
+      board.placeMarker(row, column, getActivePlayer().marker);
+      }
+      
+      printNewRound();
 
-    switchPlayerTurn();
-    printNewRound();
+      if(!isGameOver()){
+        switchPlayerTurn();
+      }
+    
   };
 
   printNewRound();
